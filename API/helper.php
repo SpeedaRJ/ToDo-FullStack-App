7d161b58f881f41acac003a0eca66e4626bcc04a
RobertJ <56695212+SpeedaRJ@users.noreply.github.com>
2022-10-02 01:09:59 +0200

    Delete API directory

--- API/helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-// function to check post/get parameters
-function safetyCheck($input)
-{
-    $input = trim($input); // strip white spaces
-    $input = stripslashes($input); // unquotes a string or strips slashes from strings
-    $input = htmlspecialchars($input); // checks for invalid characters in the given input
-    return $input;
-}
